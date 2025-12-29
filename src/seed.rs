a02d8d68068e80a775f7455b4bc3840fc2097c86
boobie <boobie@riseup.net>
2025-12-29 15:47:36 -0500

    Add HKDF debug output to diagnose non-determinism

--- src/seed.rs
+++ src/seed.rs
@@ -53,10 +53,13 @@ impl Seed {
 
     pub fn derive(&self, realm: &str, key_type: &str, length: usize) -> Zeroizing<Vec<u8>> {
         let path = format!("{VERSION}/{realm}/{key_type}");
+        eprintln!("DEBUG: HKDF path: {}", path);
+        eprintln!("DEBUG: Master key for HKDF first 8: {:?}", &self.master[..8]);
         let hk = Hkdf::<Sha256>::new(None, self.master.as_ref());
         let mut output = Zeroizing::new(vec![0u8; length]);
         hk.expand(path.as_bytes(), output.as_mut_slice())
             .expect("length should be valid");
+        eprintln!("DEBUG: HKDF output first 8: {:?}", &output[..8.min(length)]);
         output
     }
 
